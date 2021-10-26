041d8c9272c109d6eb4846f91a94b30aa377be62
Joao <cursedvictor@gmail.com>
2021-10-26 07:28:33 -0300

    ensure HttpAdapter throws error 401, 403, and 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -110,6 +110,36 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
+    test('Should return Unauthorized if post returns 401', () async {
+      mockResponse(401);
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should return Forbidden if post returns 403', () async {
+      mockResponse(403);
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.forbbiden));
+    });
+
+    test('Should return Not Found if post returns 404', () async {
+      mockResponse(404);
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.notFound));
+    });
+
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
       final future = sut.request(
